9d06fc047afa991804703ba1bb02ab1cd0f235de
Kusholkoner <kusholkoner99@gmail.com>
2026-01-20 20:19:33 +0530

    clenup

--- index.py
+++
@@ -1,6 +0,0 @@
-from api_server_vercel import app
-
-# Vercel serverless function handler
-def handler(request, context):
-    """Vercel serverless function handler"""
-    return app(request, context)
